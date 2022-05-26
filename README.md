1801eb14dd4bec44966db17fabe25681299413f1
Sowd0404 <sowdbusiness@gmail.com>
2022-05-26 23:29:35 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Black Ops 3 Fix (Custom Zombie Maps on Arch Linux / Manjaro)
+# Spoiler: This cannot fix the maps working on multiplayer (They will not)
+
+I'm unaware if this works on any other versions of linux, but let me know. 
+
+Recently I found out that custom maps don't work on arch linux and protonGE can't fix it and you need to do some really tedious manual process on all downloaded custom maps to make them even work
+
+You will know that it has worked if all the folders have now changed their names to their map names
+
+How to use: 
+
+1. Create a folder called `usermaps` inside of SteamLibrary/steamapps/common/Call Of Duty Black Ops III/ (If there isn't already one there)
+
+2. Put index.js inside of your SteamLibrary/steamappa/workshop/content/311210
+
+3. Right click the folder (or press shift + f4) to open Konsole inside of the folder and run ``node index.js``
+
+4. Select all of the folders and move them to SteamLibrary/steamapps/common/Call Of Duty Black Ops III/usermaps 
+
+5. Restart COD and enjoy 
---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+fs.readdirSync(__dirname, "utf-8").filter((file) => path.extname(file) !== ".js").forEach((entry) => {
+    const joinedPath = path.join(__dirname, entry);
+    const workshop = path.join(joinedPath, "workshop.json")
+    console.log(entry)
+
+    if (!fs.existsSync(workshop)) return console.log("error");
+    const FolderName = JSON.parse(fs.readFileSync(workshop, "utf-8")).FolderName;
+
+    fs.renameSync(joinedPath, path.join(__dirname, FolderName));
+    if (!fs.existsSync(path.join(path.join(__dirname, FolderName), "zone"))) {
+        fs.mkdirSync(path.join(__dirname, FolderName, "zone"));
+    }
+
+    fs.readdirSync(path.join(__dirname, FolderName)).forEach((workshopfile) => {
+        if (workshopfile !== "zone") {
+            const oldFile = path.join(__dirname, FolderName, workshopfile)
+            const newFile = path.join(__dirname, FolderName, "zone", workshopfile)
+            fs.renameSync(oldFile, newFile);
+        }
+    })
+});
